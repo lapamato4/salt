e57e3093844c68ba70181da770e7265103d83786
Roope <roope@salt.com>
2018-12-09 12:52:44 +0200

    mod

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ set -o verbose
 
 echo "Installing Salt and Git..."
 sudo apt-get update
-sudo apt-get -y install git salt-minion
+sudo apt-get -y install git salt-minion salt-master
 
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
@@ -16,7 +16,6 @@ sudo systemctl restart salt-minion.service
 
 git clone https://github.com/lapamato4/salt/
 cd salt/
-./highstate.sh
 
 echo "Configuring Live Desktop"
 setxkbmap fi
