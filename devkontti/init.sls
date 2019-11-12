519cad566039389814884f42eb125c05928398f2
Roope <roope.pohjanheimo@gmail.com>
2019-11-12 11:59:45 +0000

    Devkontti init.sls modified

--- devkontti/init.sls
+++ devkontti/init.sls
@@ -9,3 +9,10 @@ applications:
 /etc/docker/daemon.json:
   file.managed:
     - source: salt://devkontti/insecure_conf.json
+
+dockerservice:
+  service.running:
+    - name: docker
+    - reload: True
+    - watch:
+      - file: /etc/docker/daemon.json
--- skriptit/environment_installation.sh
+++ skriptit/environment_installation.sh
@@ -8,8 +8,8 @@ sudo apt-get -y install git salt-minion salt-master
 
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
-sudo systemctl restart salt-minion.service
 sudo salt-key -A
+sudo systemctl restart salt-minion.service
 
 cd /srv
 sudo git clone https://github.com/lapamato4/salt/
