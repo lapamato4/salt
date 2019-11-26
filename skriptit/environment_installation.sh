ef6845dc53a97dc886ecbc8acc2c72797eabe1d9
lapamato4 <43338485+lapamato4@users.noreply.github.com>
2019-11-26 08:38:58 +0000

    edit

--- skriptit/environment_installation.sh
+++ skriptit/environment_installation.sh
@@ -4,7 +4,7 @@ set -o verbose
 
 echo "Installing Salt and Git..."
 sudo apt-get update
-sudo apt-get -y install git salt-minion salt-master
+sudo apt-get -y install git curl salt-minion salt-master
 
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
